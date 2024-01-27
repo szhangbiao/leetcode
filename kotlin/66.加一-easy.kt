844518fef4fe877f7ad1c28d4c2fe003d8648f85
Barry <szhangbiao@gmail.com>
2024-01-27 12:50:49 +0800

    Leetcode 66th

---
+++ kotlin/66.加一-easy.kt
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode.cn id=66 lang=kotlin
+ *
+ * [66] 加一
+ *
+ * https://leetcode.cn/problems/plus-one/description/
+ *
+ * algorithms
+ * Easy (45.41%)
+ * Likes:    1357
+ * Dislikes: 0
+ * Total Accepted:    704.3K
+ * Total Submissions: 1.5M
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+ * 
+ * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+ * 
+ * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：digits = [1,2,3]
+ * 输出：[1,2,4]
+ * 解释：输入数组表示数字 123。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：digits = [4,3,2,1]
+ * 输出：[4,3,2,2]
+ * 解释：输入数组表示数字 4321。
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：digits = [0]
+ * 输出：[1]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * 0 
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    fun plusOne(digits: IntArray): IntArray {
+        val len = digits.size
+        for(i in len - 1 downTo 0){
+            if(i == 0 && digits[i] == 9){
+                val newDigits = IntArray(len + 1)
+                newDigits[0] = 1
+                return newDigits
+            }
+            if(digits[i] < 9) {
+                digits[i]++
+                return digits
+            } else {
+                digits[i] = 0
+            }
+        }
+        return digits
+    }
+}
+// @lc code=end
+
