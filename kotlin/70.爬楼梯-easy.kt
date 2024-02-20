0329f63b8e8f5928f44577b09ed715d57582d585
Barry <szhangbiao@gmail.com>
2024-02-20 16:27:28 +0800

    Leetcode 70

---
+++ kotlin/70.爬楼梯-easy.kt
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=kotlin
+ *
+ * [70] 爬楼梯
+ *
+ * https://leetcode.cn/problems/climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (54.33%)
+ * Likes:    3440
+ * Dislikes: 0
+ * Total Accepted:    1.4M
+ * Total Submissions: 2.5M
+ * Testcase Example:  '2'
+ *
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：n = 2
+ * 输出：2
+ * 解释：有两种方法可以爬到楼顶。
+ * 1. 1 阶 + 1 阶
+ * 2. 2 阶
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：n = 3
+ * 输出：3
+ * 解释：有三种方法可以爬到楼顶。
+ * 1. 1 阶 + 1 阶 + 1 阶
+ * 2. 1 阶 + 2 阶
+ * 3. 2 阶 + 1 阶
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= n <= 45
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    fun climbStairs(n: Int): Int {
+        var p = 0
+        var q = 0
+        var r = 1
+        for(i in 1..n){
+            p = q
+            q = r
+            r = p + q
+        }
+        return r
+    }
+}
+// @lc code=end
+
