22593f9c784faafa0f6ecf9f8b71cfe716b92667
Barry <szhangbiao@gmail.com>
2024-02-24 15:21:42 +0800

    Leetcode 100

---
+++ kotlin/100.相同的树-easy.kt
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=100 lang=kotlin
+ *
+ * [100] 相同的树
+ *
+ * https://leetcode.cn/problems/same-tree/description/
+ *
+ * algorithms
+ * Easy (60.68%)
+ * Likes:    1126
+ * Dislikes: 0
+ * Total Accepted:    534.8K
+ * Total Submissions: 878.6K
+ * Testcase Example:  '[1,2,3]\n[1,2,3]'
+ *
+ * 给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+ * 
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：p = [1,2,3], q = [1,2,3]
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：p = [1,2], q = [1,null,2]
+ * 输出：false
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：p = [1,2,1], q = [1,1,2]
+ * 输出：false
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 两棵树上的节点数目都在范围 [0, 100] 内
+ * -10^4 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+        if(p == null && q == null) return true
+        if((p?.`val` ?: -1) != (q?.`val` ?: -1)) return false
+        return isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
+    }
+}
+// @lc code=end
+
