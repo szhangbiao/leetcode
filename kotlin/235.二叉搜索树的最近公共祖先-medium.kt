92562184977ae8dfa0a0a30eda00cf5d382ad001
Barry <szhangbiao@gmail.com>
2024-02-25 17:06:30 +0800

    Leetcode 235

---
+++ kotlin/235.二叉搜索树的最近公共祖先-medium.kt
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=235 lang=kotlin
+ *
+ * [235] 二叉搜索树的最近公共祖先
+ *
+ * https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (69.20%)
+ * Likes:    1218
+ * Dislikes: 0
+ * Total Accepted:    400.5K
+ * Total Submissions: 574.8K
+ * Testcase Example:  '[6,2,8,0,4,7,9,null,null,3,5]\n2\n8'
+ *
+ * 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+ * 
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x
+ * 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ * 
+ * 例如，给定如下二叉搜索树:  root = [6,2,8,0,4,7,9,null,null,3,5]
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ * 输出: 6 
+ * 解释: 节点 2 和节点 8 的最近公共祖先是 6。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+ * 输出: 2
+ * 解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。
+ * 
+ * 
+ * 
+ * 说明:
+ * 
+ * 
+ * 所有节点的值都是唯一的。
+ * p、q 为不同节点且均存在于给定的二叉搜索树中。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int = 0) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun lowestCommonAncestor(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
+        if(root == null || root == p || root == q) return root
+        val rootVal = root?.`val` ?: -1
+        val pVal = p?.`val` ?: -1
+        val qVal = q?.`val` ?: -1
+        if(rootVal > pVal && rootVal > qVal) {
+            return lowestCommonAncestor(root?.left,p,q)
+        }
+        if(rootVal < pVal && rootVal < qVal) {
+            return lowestCommonAncestor(root?.right,p,q)
+        }
+        return root
+    }
+}
+// @lc code=end
+
