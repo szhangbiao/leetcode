332c9dd40d7a6cdc743f51bf77d6764fc9cb580e
Barry <szhangbiao@gmail.com>
2024-02-25 16:37:03 +0800

    Leetcode 226

---
+++ kotlin/226.翻转二叉树-easy.kt
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=kotlin
+ *
+ * [226] 翻转二叉树
+ *
+ * https://leetcode.cn/problems/invert-binary-tree/description/
+ *
+ * algorithms
+ * Easy (79.95%)
+ * Likes:    1771
+ * Dislikes: 0
+ * Total Accepted:    814.3K
+ * Total Submissions: 1M
+ * Testcase Example:  '[4,2,7,1,3,6,9]'
+ *
+ * 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 
+ * 
+ * 输入：root = [4,2,7,1,3,6,9]
+ * 输出：[4,7,2,9,6,3,1]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 
+ * 
+ * 输入：root = [2,1,3]
+ * 输出：[2,3,1]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：[]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数目范围在 [0, 100] 内
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun invertTree(root: TreeNode?): TreeNode? {
+        if(root == null) return null
+        val left = invertTree(root.left)
+        root.left = invertTree(root.right)
+        root.right = left
+        return root
+    }
+}
+// @lc code=end
+
