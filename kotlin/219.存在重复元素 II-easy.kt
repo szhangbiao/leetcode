7ece4eeaed5e3f5c0858ebd091eb99ba806e74bf
Barry <szhangbiao@gmail.com>
2024-02-27 23:14:25 +0800

    leetcode 219

---
+++ kotlin/219.存在重复元素 II-easy.kt
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=219 lang=kotlin
+ *
+ * [219] 存在重复元素 II
+ *
+ * https://leetcode.cn/problems/contains-duplicate-ii/description/
+ *
+ * algorithms
+ * Easy (45.48%)
+ * Likes:    688
+ * Dislikes: 0
+ * Total Accepted:    278.9K
+ * Total Submissions: 609.1K
+ * Testcase Example:  '[1,2,3,1]\n3'
+ *
+ * 给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个 不同的索引 i 和 j ，满足 nums[i] == nums[j] 且
+ * abs(i - j) <= k 。如果存在，返回 true ；否则，返回 false 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [1,2,3,1], k = 3
+ * 输出：true
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [1,0,1,1], k = 1
+ * 输出：true
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：nums = [1,2,3,1,2,3], k = 2
+ * 输出：false
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= nums.length <= 10^5
+ * -10^9 <= nums[i] <= 10^9
+ * 0 <= k <= 10^5
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    fun containsNearbyDuplicate(nums: IntArray, k: Int): Boolean {
+        val map = mutableMapOf<Int, Int>()
+        for(i in nums.indices){
+            if(map.containsKey(nums[i]) && i - map[nums[i]]!! <= k){
+                return true
+            }
+            map[nums[i]] = i
+        }
+        return false
+    }
+}
+// @lc code=end
+
