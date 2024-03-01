d81d4ddf9d29c3a58aea5d9e7de319a00225193f
Barry <szhangbiao@gmail.com>
2024-03-01 17:04:13 +0800

    leetcode 128

---
+++ kotlin/128.最长连续序列-medium.kt
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode.cn id=128 lang=kotlin
+ *
+ * [128] 最长连续序列
+ *
+ * https://leetcode.cn/problems/longest-consecutive-sequence/description/
+ *
+ * algorithms
+ * Medium (52.75%)
+ * Likes:    1988
+ * Dislikes: 0
+ * Total Accepted:    553.3K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[100,4,200,1,3,2]'
+ *
+ * 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+ * 
+ * 请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [100,4,200,1,3,2]
+ * 输出：4
+ * 解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [0,3,7,2,5,8,4,6,0,1]
+ * 输出：9
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 
+ * -10^9 
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    fun longestConsecutive(nums: IntArray): Int {
+        if(nums.size < 2) return nums.size
+        val set = nums.toSet()
+        var ans = 0
+        for(num in nums){
+            if(!set.contains(num-1)){
+                var cur = num
+                var curAns = 1
+                while(set.contains(cur+1)){
+                    cur++
+                    curAns++
+                }
+                ans = maxOf(ans, curAns)
+            }
+        }
+        return ans
+    }
+}
+// @lc code=end
+
