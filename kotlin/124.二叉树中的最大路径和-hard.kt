28460132d51d29d9a20d107cc153921ce80db0b9
Barry <szhangbiao@gmail.com>
2024-03-05 11:03:36 +0800

    leetcode 124

---
+++ kotlin/124.二叉树中的最大路径和-hard.kt
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode.cn id=124 lang=kotlin
+ *
+ * [124] 二叉树中的最大路径和
+ *
+ * https://leetcode.cn/problems/binary-tree-maximum-path-sum/description/
+ *
+ * algorithms
+ * Hard (45.37%)
+ * Likes:    2176
+ * Dislikes: 0
+ * Total Accepted:    382.3K
+ * Total Submissions: 841.6K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 二叉树中的 路径 被定义为一条节点序列，序列中每对相邻节点之间都存在一条边。同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个
+ * 节点，且不一定经过根节点。
+ * 
+ * 路径和 是路径中各节点值的总和。
+ * 
+ * 给你一个二叉树的根节点 root ，返回其 最大路径和 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [1,2,3]
+ * 输出：6
+ * 解释：最优路径是 2 -> 1 -> 3 ，路径和为 2 + 1 + 3 = 6
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = [-10,9,20,null,null,15,7]
+ * 输出：42
+ * 解释：最优路径是 15 -> 20 -> 7 ，路径和为 15 + 20 + 7 = 42
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数目范围是 [1, 3 * 10^4]
+ * -1000 <= Node.val <= 1000
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    var maxPath: Int = Int.MIN_VALUE
+    fun maxPathSum(root: TreeNode?): Int {
+        dfs(root)
+        return maxPath
+    }
+
+    fun dfs(root: TreeNode?): Int {
+        if(root == null) return 0
+        val leftMax = Math.max(dfs(root?.left),0)
+        val rightMax = Math.max(dfs(root.right),0)
+        val max = root?.`val`!! + leftMax + rightMax
+        maxPath = Math.max(maxPath,max)
+        return Math.max(leftMax, rightMax) + root?.`val`!!
+    }
+}
+// @lc code=end
+
