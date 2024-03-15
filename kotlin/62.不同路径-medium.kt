272a63a01305999cf67d8349b1938f6c1e62482d
Barry <szhangbiao@gmail.com>
2024-03-15 12:07:39 +0800

    leetcode 62

---
+++ kotlin/62.不同路径-medium.kt
@@ -0,0 +1,97 @@
+/*
+ * @lc app=leetcode.cn id=62 lang=kotlin
+ *
+ * [62] 不同路径
+ *
+ * https://leetcode.cn/problems/unique-paths/description/
+ *
+ * algorithms
+ * Medium (67.94%)
+ * Likes:    2003
+ * Dislikes: 0
+ * Total Accepted:    746.4K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '3\n7'
+ *
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+ * 
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+ * 
+ * 问总共有多少条不同的路径？
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：m = 3, n = 7
+ * 输出：28
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：m = 3, n = 2
+ * 输出：3
+ * 解释：
+ * 从左上角开始，总共有 3 条路径可以到达右下角。
+ * 1. 向右 -> 向下 -> 向下
+ * 2. 向下 -> 向下 -> 向右
+ * 3. 向下 -> 向右 -> 向下
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：m = 7, n = 3
+ * 输出：28
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：m = 3, n = 3
+ * 输出：6
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= m, n <= 100
+ * 题目数据保证答案小于等于 2 * 10^9
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {fun uniquePaths(m: Int, n: Int): Int {
+        val dp = IntArray(n)
+        for(i in 0 until n){
+            dp[i] = 1
+        }
+        for (i in 1 until m) {
+            for (j in 1 until n) {
+                dp[j] += dp[j - 1]
+            }
+        }
+        return dp[n - 1]
+    }
+    
+    fun uniquePaths1(m: Int, n: Int): Int {
+        val dp = Array(m) { IntArray(n) }
+        for (i in 0 until m) {
+            dp[i][0] = 1
+        }
+        for (j in 0 until n) {
+            dp[0][j] = 1
+        }
+        for (i in 1 until m) {
+            for (j in 1 until n) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+            }
+        }
+        return dp[m - 1][n - 1]
+    }
+}
+// @lc code=end
+
