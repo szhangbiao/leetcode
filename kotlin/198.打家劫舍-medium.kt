3fb8d95d72fffb8685291569ef289be085135b18
Barry <szhangbiao@gmail.com>
2024-03-17 22:01:09 +0800

    update 198

--- kotlin/198.打家劫舍-medium.kt
+++ kotlin/198.打家劫舍-medium.kt
@@ -51,6 +51,19 @@
 // @lc code=start
 class Solution {
     fun rob(nums: IntArray): Int {
+        if(nums.isEmpty()) return 0
+        if(nums.size == 1) return nums[0]
+        if(nums.size == 2) return Math.max(nums[0], nums[1])
+        val dp = IntArray(nums.size)
+        dp[0] = nums[0]
+        dp[1] = Math.max(nums[0], nums[1])
+        for(i in 2 until nums.size){
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1])
+        }
+        return dp[nums.size - 1]
+    }
+
+    fun rob1(nums: IntArray): Int {
         if(nums.size ==0) return 0
         val n = nums.size
         if(n == 1) return nums[0]
